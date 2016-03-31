c2b23de2b8d2089b0b48cd12aedcc97668767624
ColmBreen <C14542587@mydit.ie>
2016-03-31 18:25:26 +0100

    Further Enemy attacks

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -9,11 +9,11 @@ public class EnemyBehaviour : MonoBehaviour {
 	public float speed;
 	private int direction;
 	public int health;
-	private bool wall = false;
-	private bool shootL = false, shootR = false;
+	private bool wall;
+	private bool shootL, shootR;
 	public Transform ShotSpawn;
 	public float fireRate = 0.5f;
-	private float nextFire = 0.0f;
+	public float nextFire = 0.0f;
 	public GameObject Bullet;
 	
 	void Start()
@@ -22,49 +22,53 @@ public class EnemyBehaviour : MonoBehaviour {
 		startPos = erb.transform.position.x;
 		direction = -1;
 		health = 50;
+		wall = false;
+		shootL = false; 
+		shootR = false;
 	}
 	
 	void Update()
 	{
 		GameObject player = GameObject.FindWithTag("Player");
 		PlayerControl playerC = player.GetComponent<PlayerControl>();
-		if((playerC.posX + 10) > erb.transform.position.x)
-		{
-			shootL = true;
-		}
-		if((playerC.posX - 10) < erb.transform.position.x)
+		if((playerC.posX - 10) < erb.transform.position.x && playerC.posX > erb.transform.position.x)
 		{
 			shootR = true;
+			erb.velocity = new Vector2(0, 0);
+			if(Time.time > nextFire)
+			{
+				nextFire = Time.time + fireRate;
+				Instantiate (Bullet, ShotSpawn.position, ShotSpawn.rotation);
+			}
+		}
+		else if((playerC.posX + 10) > erb.transform.position.x && playerC.posX < erb.transform.position.x)
+		{
+			shootL = true;
+			erb.velocity = new Vector2(0, 0);
+			if(Time.time > nextFire)
+			{
+				nextFire = Time.time + fireRate;
+				Instantiate (Bullet, (ShotSpawn.position + (Vector3.left + Vector3.left + Vector3.left)), ShotSpawn.rotation);
+			}
+		}
+		else
+		{
+			shootL = false;
+			shootR = false;
 		}
 	}
 	
 	void FixedUpdate()
 	{
-		if(erb.transform.position.x > (startPos - 10) && direction < 0 && wall == false)// && shootR == false && shootL == false)
+		if(erb.transform.position.x > (startPos - 10) && direction < 0 && wall == false && shootR == false && shootL == false)
 		{
 			moveVelocity = -speed;
 			erb.velocity = new Vector2(moveVelocity, erb.velocity.y);
 		}
-		else if(erb.transform.position.x < (startPos + 10) && direction > 0 && wall == false)// && shootR == false && shootL == false)
+		else if(erb.transform.position.x < (startPos + 10) && direction > 0 && wall == false && shootR == false && shootL == false)
 		{
 			moveVelocity = speed;
 			erb.velocity = new Vector2(moveVelocity, erb.velocity.y);
-		}
-		else if(shootL == true)
-		{
-			nextFire = Time.time + fireRate;
-			if(Time.time > nextFire)
-			{
-				Instantiate (Bullet, ShotSpawn.position, ShotSpawn.rotation);
-			}
-		}
-		else if(shootR == true)
-		{
-			nextFire = Time.time + fireRate;
-			if(Time.time > nextFire)
-			{
-				Instantiate (Bullet, (ShotSpawn.position + (Vector3.left + (Vector3.left/2))), ShotSpawn.rotation);
-			}
 		}
 		else
 		{
