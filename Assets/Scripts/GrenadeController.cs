dfaf8a497bddde619ce30306eb496c835b5add9e
ColmBreen <C14542587@mydit.ie>
2016-04-17 13:16:48 +0100

    Grenades working

--- Assets/Scripts/GrenadeController.cs
+++ Assets/Scripts/GrenadeController.cs
@@ -6,35 +6,54 @@ public class GrenadeController : MonoBehaviour {
 	private float grenadeSpeed;
 	private float cook;
 	private Rigidbody2D rb;
+	private bool playerDead;
+	
+	public GameObject bloodParticles;
+	public GameObject dirtParticles;
 	
 	void Start()
 	{
-		grenadeSpeed = 20;
+		playerDead = false;
+		grenadeSpeed = 10;
 		rb = GetComponent<Rigidbody2D>();
 		if(GM.instance.playerDirection == 1)
 		rb.velocity = new Vector2(grenadeSpeed, grenadeSpeed);
 		else
 			rb.velocity = new Vector2(grenadeSpeed * -1, grenadeSpeed);
 		GM.instance.playerFire = false;
-		cook = Time.time + 5f;
+		cook = Time.time + 2f;
 	}
 	
 	void Update()
 	{
 		if(Time.time > cook)
 		{
-			Explosion(rb.transform.position, 15f);
+			Explosion(rb.transform.position, 2f);
 		}
 	}
 	
-	void Explosion(Vector3 centre, float radius)
+	void Explosion(Vector2 centre, float radius)
 	{
-		int i = 0;
-		Collider[] colliders = Physics.OverlapSphere(centre, radius);
-		while(i < colliders.Length)
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(centre, radius, 1, -1f, 1f);
+		foreach(Collider2D objects in colliders)
 		{
-			colliders[i].grenadeDamage();
-			i++;
+			if(objects.gameObject.tag == "Player" && playerDead == false)
+			{
+				playerDead = true;
+				GM.instance.LoseLife();
+			}
+			if(objects.gameObject.tag == "Enemy")
+			{
+				Instantiate(bloodParticles, objects.transform.position, Quaternion.identity);
+				Destroy(objects.gameObject);
+			}
+			if(objects.gameObject.tag == "Dirt")
+			{
+				Instantiate(dirtParticles, objects.transform.position, Quaternion.identity);
+				Destroy(objects.gameObject);
+			}	
+			//colliders[i].gameObject.grenadeDamage();
 		}
+		Destroy(gameObject);
 	}
 }
